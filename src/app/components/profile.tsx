7f207787641df05ccf0606d1fabff2a8a3a90d4b
t4ni_XP <gutchi-t@outlook.com>
2025-01-02 14:18:06 +0900

    fix image size

--- src/app/components/profile.tsx
+++ src/app/components/profile.tsx
@@ -7,7 +7,7 @@ export default function Profile() {
       <h1 className="text-3xl font-bold mb-1">PROFILE</h1>
       <div className="flex items-center mr-10 mb-10 ml-0 space-x-8">
         {/* 画像をラップしているコンテナ */}
-        <div className="flex items-center justify-center h-44 w-44 overflow-hidden">
+        <div className="flex items-center justify-center h-40 w-40 overflow-hidden">
           <Image
             src="/MizukiTaniguchi.jpeg"
             alt="Mizuki Taniguchi"
