4c58374769ea7ab5868a1621a13b9815c69499fe
t4ni_XP <gutchi-t@outlook.com>
2025-01-02 11:28:06 +0900

    fix

--- src/app/components/interests.tsx
+++ src/app/components/interests.tsx
@@ -1,15 +1,25 @@
 export default function Interests() {
-    return (
-        <div className="mt-10">
-            <h1 className="text-3xl font-bold mb-4 ml-10">INTERESTS</h1>
-            <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 ml-10">
-                <div className="bg-gray-800 text-gray-100 p-4 rounded-lg">Web Development</div>
-                <div className="bg-gray-800 text-gray-100 p-4 rounded-lg">Orienteering</div>
-                <div className="bg-gray-800 text-gray-100 p-4 rounded-lg">UI/UX Design</div>
-                <div className="bg-gray-800 text-gray-100 p-4 rounded-lg">Cybersecurity</div>
-                <div className="bg-gray-800 text-gray-100 p-4 rounded-lg">Music</div>
-                <div className="bg-gray-800 text-gray-100 p-4 rounded-lg">Driving</div>
-            </div>
+  return (
+    <div className="m-10">
+      <h1 className="text-3xl font-bold mb-4">INTERESTS</h1>
+      <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
+        <div className="bg-gray-600 text-gray-100 p-4 rounded-lg">
+          Web Development 💻
         </div>
-    );
+        <div className="bg-gray-600 text-gray-100 p-4 rounded-lg">
+          Orienteering 🗺️
+        </div>
+        <div className="bg-gray-600 text-gray-100 p-4 rounded-lg">
+          UI/UX Design 🎨
+        </div>
+        <div className="bg-gray-600 text-gray-100 p-4 rounded-lg">
+          Cybersecurity 🔒
+        </div>
+        <div className="bg-gray-600 text-gray-100 p-4 rounded-lg">Music 🎵</div>
+        <div className="bg-gray-600 text-gray-100 p-4 rounded-lg">
+          Driving 🚗
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/components/profile.tsx
+++ src/app/components/profile.tsx
@@ -2,32 +2,34 @@ import React from 'react';
 import Image from 'next/image';
 
 export default function Profile() {
-    return (
-        <div className='h-64 w-fill mt-10 ml-10 rounded-lg'>
-            <h1 className='text-3xl font-bold mb-1'>PROFILE</h1>
-            <div className="flex items-center mr-10 mb-10 ml-0 space-x-8">
-                {/* 画像をラップしているコンテナ */}
-                <div className="flex items-center justify-center h-44 w-44 overflow-hidden">
-                    <Image
-                        src="/MizukiTaniguchi.jpeg"
-                        alt="Mizuki Taniguchi"
-                        className="rounded-full shadow-lg"
-                        width={192}
-                        height={192}
-                    />
-                </div>
-
-                {/* プロフィールテキスト */}
-                <div className="flex flex-col">
-                    <h1 className="text-3xl font-bold mb-4">Mizuki Taniguchi</h1>
-                    <p className="text-gray-300">
-                        {/* Web Developer. Orienteer. \n Currently studying at University of Tsukuba. */}
-                        <span className="block">Web Developer.</span>
-                        <span className="block">Orienteer.</span>
-                        <span className="block">Currently studying at University of Tsukuba.</span>
-                    </p>
-                </div>
-            </div>
+  return (
+    <div className="h-64 w-fill mt-10 ml-10 rounded-lg">
+      <h1 className="text-3xl font-bold mb-1">PROFILE</h1>
+      <div className="flex items-center mr-10 mb-10 ml-0 space-x-8">
+        {/* 画像をラップしているコンテナ */}
+        <div className="flex items-center justify-center h-44 w-44 overflow-hidden">
+          <Image
+            src="/MizukiTaniguchi.jpeg"
+            alt="Mizuki Taniguchi"
+            className="rounded-full shadow-lg"
+            width={192}
+            height={192}
+          />
         </div>
-    );
+
+        {/* プロフィールテキスト */}
+        <div className="flex flex-col">
+          <h1 className="text-3xl font-bold mb-4">Mizuki Taniguchi</h1>
+          <p className="text-gray-300">
+            {/* Web Developer. Orienteer. \n Currently studying at University of Tsukuba. */}
+            <span className="block">Web Developer.</span>
+            <span className="block">Orienteer.</span>
+            <span className="block">
+              Currently studying at University of Tsukuba.
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
