46ab1eff99b494c23e2f7e08f5b4b2fc83563e28
t4ni_XP <gutchi-t@outlook.com>
2025-01-02 12:57:38 +0900

    aa

--- src/app/components/interests.tsx
+++ src/app/components/interests.tsx
@@ -1,19 +1,19 @@
 export default function Interests() {
   return (
     <div className="m-10">
-      <h1 className="text-3xl font-bold mb-4">INTERESTS</h1>
+      <h1 className="text-3xl font-bold mb-4">INTEREST</h1>
       <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
         <div className="bg-gray-700 text-gray-100 p-4 rounded-lg">
           Web Development 💻
-        </div>
-        <div className="bg-gray-700 text-gray-100 p-4 rounded-lg">
-          Orienteering 🗺️
         </div>
         <div className="bg-gray-700 text-gray-100 p-4 rounded-lg">
           UI/UX Design 🎨
         </div>
         <div className="bg-gray-700 text-gray-100 p-4 rounded-lg">
           Cybersecurity 🔒
+        </div>
+        <div className="bg-gray-700 text-gray-100 p-4 rounded-lg">
+          Orienteering 🗺️
         </div>
         <div className="bg-gray-700 text-gray-100 p-4 rounded-lg">Music 🎵</div>
         <div className="bg-gray-700 text-gray-100 p-4 rounded-lg">
