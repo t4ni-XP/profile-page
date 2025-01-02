cb86f83e22ce473d87235fe35bc9098af25639d9
t4ni_XP <gutchi-t@outlook.com>
2025-01-02 11:28:44 +0900

    fix

--- src/app/components/interests.tsx
+++ src/app/components/interests.tsx
@@ -3,20 +3,20 @@ export default function Interests() {
     <div className="m-10">
       <h1 className="text-3xl font-bold mb-4">INTERESTS</h1>
       <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
-        <div className="bg-gray-600 text-gray-100 p-4 rounded-lg">
+        <div className="bg-gray-700 text-gray-100 p-4 rounded-lg">
           Web Development 💻
         </div>
-        <div className="bg-gray-600 text-gray-100 p-4 rounded-lg">
+        <div className="bg-gray-700 text-gray-100 p-4 rounded-lg">
           Orienteering 🗺️
         </div>
-        <div className="bg-gray-600 text-gray-100 p-4 rounded-lg">
+        <div className="bg-gray-700 text-gray-100 p-4 rounded-lg">
           UI/UX Design 🎨
         </div>
-        <div className="bg-gray-600 text-gray-100 p-4 rounded-lg">
+        <div className="bg-gray-700 text-gray-100 p-4 rounded-lg">
           Cybersecurity 🔒
         </div>
-        <div className="bg-gray-600 text-gray-100 p-4 rounded-lg">Music 🎵</div>
-        <div className="bg-gray-600 text-gray-100 p-4 rounded-lg">
+        <div className="bg-gray-700 text-gray-100 p-4 rounded-lg">Music 🎵</div>
+        <div className="bg-gray-700 text-gray-100 p-4 rounded-lg">
           Driving 🚗
         </div>
       </div>
