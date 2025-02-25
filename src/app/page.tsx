44dfb7fadab5c1e94ecf1afacb2f26f43b20344c
t4ni_XP <gutchi-t@outlook.com>
2025-02-25 23:06:08 +0900

    fix switch position and format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-"use client";
+'use client';
 
-import { useDarkTheme } from "./contexts/darkThemeContext";
-import DarkModeSwitcher from "./components/darkModeSwitcher";
-import Profile from "./components/profile";
-import Interests from "./components/interests";
-import TechStack from "./components/techStacks";
-import Career from "./components/career";
-import Link from "next/link";
+import { useDarkTheme } from './contexts/darkThemeContext';
+import DarkModeSwitcher from './components/darkModeSwitcher';
+import Profile from './components/profile';
+import Interests from './components/interests';
+import TechStack from './components/techStacks';
+import Career from './components/career';
+import Link from 'next/link';
 
 export default function Home() {
   // ① useDarkTheme からダークテーマフラグを取得
@@ -17,14 +17,15 @@ export default function Home() {
       <body className="flex justify-center">
         <div
           className={`relative flex flex-col items-center w-full sm:w-2/3 min-h-screen sm:m-24 sm:rounded-lg 
-      ${darkTheme == "dark"
-              ? "bg-dark-pink text-foreground-primary" // ダークテーマの場合
-              : "bg-pink-horizon text-foreground-primary"        // ライトテーマの場合
-            }
+      ${
+        darkTheme == 'dark'
+          ? 'bg-dark-pink text-foreground-primary' // ダークテーマの場合
+          : 'bg-pink-horizon text-foreground-primary' // ライトテーマの場合
+      }
       `}
         >
           {/* 右上に言語切替ボタンを絶対配置 */}
-          <div className="absolute top-0 right-0 m-4 flex items-center">
+          <div className="absolute top-0 right-0 m-4 flex flex-col items-end md:flex-row md:items-center">
             <DarkModeSwitcher />
             <div className="ml-2 mb-2">
               <span className="text-gray-400">EN</span>
